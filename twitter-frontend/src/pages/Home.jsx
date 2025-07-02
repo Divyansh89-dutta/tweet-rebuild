904e5cfed6bf10c85a2a20f7438ae4446a1df1dd
Divyansh89-dutta <divyanshduttaroy163@gmail.com>
2025-07-02 21:38:28 +0530

    feat: implement Home component to fetch and display tweets from timeline

---
+++ twitter-frontend/src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Home() {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    async function fetchTweets() {
+      try {
+        const token = localStorage.getItem("token");
+        const res = await axios.get("/api/tweet/timeline", {
+          header: { Authorization: `Bearer ${token}` },
+        });
+        console.log("Fetched Tweets:", res.data);
+        setTweets(res.data.data || res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchTweets();
+  }, []);
+  return <div>
+    <h2>Home feed</h2>
+    <ul>
+        {Array.isArray(tweets) &&
+          tweets.map((tweet) => (
+            <li key={tweet._id}>
+              <strong>{tweet.user?.username}:</strong> {tweet.text}
+            </li>
+          ))}
+    </ul>
+  </div>;
+}
+
+export default Home;
